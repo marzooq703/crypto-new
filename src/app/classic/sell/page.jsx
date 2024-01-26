885b160e95bf9347f493ccf2ed847054a8117532
Aqeel703 <ahamed703aqeel@gmail.com>
2024-01-27 01:21:51 +0530

    change some function in the crypto trns flow and add the balances checking function and check the error

--- src/app/classic/sell/page.jsx
+++ src/app/classic/sell/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import cn from 'classnames';
 import Button from '@/components/ui/button';
@@ -44,99 +45,107 @@ const startPayment = async ({ setError, setTxs, amount, addr, network }) => {
     throw err;
   }
 };
-
+const usdtContractAbi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', type: 'string' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', type: 'string' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', type: 'uint8' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_spender', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ name: 'success', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '_owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_to', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: 'success', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_from', type: 'address' },
+      { name: '_to', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: 'success', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
 const sendUSDT = async (signer, toAddress, amount) => {
   try {
     const web3 = new Web3(window.ethereum);
+    const usdtMaticContractAddress =
+      '0xc2132d05d31c914a87c6611c10748aeb04b58e8f';
+    const usdtEthContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+    const usdtBnbContractAddress = '0x55d398326f99059ff775485246999027b3197955';
 
-    const usdtContractAddress = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+    // const usdtContractAddress =
+    //   network === 'matic'
+    //     ? usdtMaticContractAddress
+    //     : network === 'eth'
+    //     ? usdtEthContractAddress
+    //     : network === 'bnb'
+    //     ? usdtBnbContractAddress
+    //     : '';
 
-    const usdtContractAbi = [
-      {
-        constant: true,
-        inputs: [],
-        name: 'name',
-        outputs: [{ name: '', type: 'string' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: 'symbol',
-        outputs: [{ name: '', type: 'string' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: 'decimals',
-        outputs: [{ name: '', type: 'uint8' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: false,
-        inputs: [
-          { name: '_spender', type: 'address' },
-          { name: '_value', type: 'uint256' },
-        ],
-        name: 'approve',
-        outputs: [{ name: 'success', type: 'bool' }],
-        payable: false,
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        constant: true,
-        inputs: [{ name: '_owner', type: 'address' }],
-        name: 'balanceOf',
-        outputs: [{ name: 'balance', type: 'uint256' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: false,
-        inputs: [
-          { name: '_to', type: 'address' },
-          { name: '_value', type: 'uint256' },
-        ],
-        name: 'transfer',
-        outputs: [{ name: 'success', type: 'bool' }],
-        payable: false,
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        constant: false,
-        inputs: [
-          { name: '_from', type: 'address' },
-          { name: '_to', type: 'address' },
-          { name: '_value', type: 'uint256' },
-        ],
-        name: 'transferFrom',
-        outputs: [{ name: 'success', type: 'bool' }],
-        payable: false,
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-    ];
     const usdtContract = new web3.eth.Contract(
       usdtContractAbi,
-      usdtContractAddress,
+      usdtMaticContractAddress,
     );
 
-    const amountWei = web3.utils.toWei(amount, 'ether');
-    const amountWei1 = web3.utils.toWei(amount, 'mwei');
-
-    const gasPriceWei = await web3.eth.getGasPrice();
+    const amountWei = web3.utils.toWei(amount, 'ether'); //bnb
+    const amountWei1 = web3.utils.toWei(amount, 'mwei'); // matic eth
+    const gasPriceWei = await web3.eth.getGasPrice(); // Dynamically fetch current gas price
     const gasPriceHex = web3.utils.toHex(gasPriceWei);
-
     const gasLimit = 50000;
 
     await usdtContract.methods.transfer(toAddress, amountWei1).send({
@@ -150,6 +159,7 @@ const sendUSDT = async (signer, toAddress, amount) => {
     throw error;
   }
 };
+
 const getChainId = (network) => {
   switch (network) {
     case 'eth':
@@ -166,24 +176,78 @@ const SellCrypto = () => {
   let [toggleCoin, setToggleCoin] = useState(false);
 
   const [sellingAmount, setSellingAmount] = useState({});
+  const [usdtBalance, setUsdtBalance] = useState({});
+
   const [cryptoAmount, setCryptoAmount] = useState({});
   const [, setError] = useState();
   const [, setTxs] = useState([]);
 
   const cryptowalletAmount = JSON.stringify(cryptoAmount.amount);
   console.log(cryptowalletAmount, 'hjh');
+
+  useEffect(() => {
+    const fetchUsdtBalance = async () => {
+      const web3 = new Web3(window.ethereum);
+
+      try {
+        if (!window.ethereum) {
+          throw new Error('No crypto wallet found. Please install MetaMask.');
+        }
+        const usdtMaticContractAddress =
+          '0xc2132d05d31c914a87c6611c10748aeb04b58e8f';
+        const usdtEthContractAddress =
+          '0xdac17f958d2ee523a2206206994597c13d831ec7';
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
+        console.log('Wallet Address:', address);
+
+        const usdtContract = new web3.eth.Contract(
+          usdtContractAbi,
+          usdtMaticContractAddress,
+        );
+
+        const usdtBalanceWei = await usdtContract.methods
+          .balanceOf(address)
+          .call();
+
+        const usdtBalance = parseFloat(
+          web3.utils.fromWei(usdtBalanceWei, 'ether'),
+        );
+        const Balance = `${usdtBalance}`.slice(0, -4);
+
+        setUsdtBalance(Balance);
+
+        console.log('Initial USDT Balance:', Balance);
+      } catch (error) {
+        console.error('Error fetching USDT balance', error.message);
+      }
+    };
+
+    fetchUsdtBalance();
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(null);
-
+    const sellingAmountValue = parseFloat(sellingAmount.value);
+    console.log(sellingAmountValue, 'sellingAmountValue');
+    console.log(usdtBalance, 'usdtBalance');
     try {
-      await startPayment({
-        setError,
-        setTxs,
-        amount: `${sellingAmount.value}`,
-        addr: '0xb141A92Eabd9F05D21bB388a8AFfcA6d6Eea752B',
-        network: 'matic', //  "eth", "matic", "bnb",
-      });
+      if (sellingAmountValue > usdtBalance) {
+        window.alert('Insufficient balance. Please enter a valid amount.');
+      } else {
+        await startPayment({
+          setError,
+          setTxs,
+          amount: `${sellingAmount.value}`,
+          addr: '0xb141A92Eabd9F05D21bB388a8AFfcA6d6Eea752B',
+          network: 'matic', //  "eth", "matic", "bnb",
+        });
+      }
+
       // navigate("/sell-crypto-details");
     } catch (err) {
       setError(err.message);
