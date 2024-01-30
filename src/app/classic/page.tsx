e1c7c5df720fd891814fd81da601f2c30ea3f5c2
TARIQ_AHMED <tariqahmed1102@gmail.com>
2024-01-30 11:50:26 +0530

    have setted redirection

--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -8,10 +8,9 @@ export default function IndexPageClassic() {
   const router = useRouter();
 
   useEffect(() => {
-    // Check for authentication or other conditions before allowing access
     const isAuthenticated = localStorage.getItem('auth');
     if (!isAuthenticated) {
-      router.push('/signin'); // Redirect to sign-in page if not authenticated
+      router.push('/authentication');
     }
   }, []);
 
