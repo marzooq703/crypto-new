6b8ca0b1fd3fffc49f544c9ef35d2c071d314363
unknown <tariqahmed1102@gmail.com>
2024-02-03 22:04:25 +0530

    removed GeoFencing

--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import GeoFencing from '@/components/geoFencing/page';
+// import GeoFencing from '@/components/geoFencing/page';
 import ClassicScreen from '@/components/screens/classic-screen';
 
 export default function IndexPageClassic() {
   return (
     <>
-      <GeoFencing />
+      {/* <GeoFencing /> */}
       <ClassicScreen />;
     </>
   );
