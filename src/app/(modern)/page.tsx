b7588329c8e496152b8bdaa9fa18049a3df5eb05
unknown <tariqahmed1102@gmail.com>
2024-02-05 20:13:22 +0530

    have completed sign in

--- src/app/(modern)/page.tsx
+++ src/app/(modern)/page.tsx
@@ -2,14 +2,24 @@
 import ModernScreen from '@/components/screens/modern-screen';
 import ClassicScreen from '@/components/screens/classic-screen';
 import { useEffect } from 'react';
+import { auth } from '../../lib/firebase';
 import { useRouter } from 'next/navigation';
+
+// Import your screen components here
+
 export default function IndexPageModern() {
   const router = useRouter();
-  // useEffect(() => {
-  //   const isAuthenticated = localStorage.getItem('auth');
-  //   if (!isAuthenticated) {
-  //     router.push('/authentication');
-  //   }
-  // }, []);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        // If user is not authenticated, redirect to the authentication page
+        router.push('/authentication');
+      }
+    });
+
+    // Unsubscribe from the auth state listener to avoid memory leaks
+    return () => unsubscribe();
+  }, [router]);
+
   return <ClassicScreen />;
 }
--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -1,8 +1,24 @@
 'use client';
 // import GeoFencing from '@/components/geoFencing/page';
 import ClassicScreen from '@/components/screens/classic-screen';
+import { useEffect } from 'react';
+import { auth } from '../../lib/firebase';
+import { useRouter } from 'next/navigation';
 
 export default function IndexPageClassic() {
+  const router = useRouter();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        // If user is not authenticated, redirect to the authentication page
+        router.push('/authentication');
+      }
+    });
+
+    // Unsubscribe from the auth state listener to avoid memory leaks
+    return () => unsubscribe();
+  }, [router]);
+
   return (
     <>
       {/* <GeoFencing /> */}
