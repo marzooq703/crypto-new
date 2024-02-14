c6b54765115d04c4fcdbcf850f7c35efc1abba8c
Hassan Marzooq <marzooq703@gmail.com>
2024-02-15 02:55:17 +0530

    completed KYC for Aadhar

--- src/app/classic/kyc/page.jsx
+++ src/app/classic/kyc/page.jsx
@@ -1,21 +1,41 @@
 'use client';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Button from '@/components/ui/button/button';
+import { useRouter } from 'next/navigation';
 
 const KYC = () => {
-  useEffect(() => {
-    axios
-      .get('/classic/kyc/api')
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <>
-      <div>KYC</div>
+      <Button
+        shape="rounded"
+        disabled={isLoading}
+        onClick={() => {
+          setIsLoading(true);
+          axios
+            .get('/classic/kyc/api')
+            .then((response) => {
+              console.log(response);
+              const url = response.data.res.url;
+              if (url) {
+                alert(url);
+                router.push(url);
+              } else {
+                alert('Problem proceeding with KYC');
+              }
+              setIsLoading(false);
+            })
+            .catch((error) => {
+              console.error(error);
+              setIsLoading(false);
+            });
+        }}
+      >
+        Complete Aadhar KYC
+      </Button>
     </>
   );
 };
