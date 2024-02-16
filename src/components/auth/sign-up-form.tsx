8f14627611a07a3f97384ddd5e594d36bef11924
unknown <tariqahmed1102@gmail.com>
2024-02-16 11:48:28 +0530

    have added checkbox verification.

--- src/components/auth/sign-up-form.tsx
+++ src/components/auth/sign-up-form.tsx
@@ -26,10 +26,16 @@ export default function SignUpForm() {
   const [password, setPassword] = useState('');
   const [contactNumber, setContactNumber] = useState('');
   const [agreeChecked, setAgreeChecked] = useState(false);
+  const [allFieldsFilled, setAllFieldsFilled] = useState(false);
   const [signUpStatus, setSignUpStatus] = useState<SignUpStatus>(null); // Track sign-in status
 
   const signUp = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!allFieldsFilled) {
+      alert('Please fill in all the fields.');
+      return;
+    }
 
     if (!agreeChecked) {
       alert('Please agree to the terms and conditions.');
@@ -57,6 +63,13 @@ export default function SignUpForm() {
       });
   };
 
+  const handleInputChange = () => {
+    if (firstName && lastName && email && password && contactNumber) {
+      setAllFieldsFilled(true);
+    } else {
+      setAllFieldsFilled(false);
+    }
+  };
   // function handleSubmit(e: any) {
   //   e.preventDefault();
   //   console.log(e);
@@ -79,14 +92,20 @@ export default function SignUpForm() {
           placeholder="First Name"
           inputClassName="focus:!ring-0 placeholder:text-[#6B7280]"
           value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+            handleInputChange();
+          }}
         />
         <Input
           type="text"
           placeholder="Last Name"
           inputClassName="focus:!ring-0 placeholder:text-[#6B7280]"
           value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
+          onChange={(e) => {
+            setLastName(e.target.value);
+            handleInputChange();
+          }}
         />
       </div>
       <Input
@@ -94,14 +113,19 @@ export default function SignUpForm() {
         placeholder="Email"
         inputClassName="focus:!ring-0 placeholder:text-[#6B7280]"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          handleInputChange();
+        }}
       />
       <Input
         type="text"
         placeholder="Contact Number"
         inputClassName="focus:!ring-0 placeholder:text-[#6B7280]"
         value={contactNumber}
-        onChange={(e) => setContactNumber(e.target.value)}
+        onChange={(e) => {
+          setContactNumber(e.target.value), handleInputChange();
+        }}
       />
       <div className="relative">
         <Input
@@ -109,7 +133,9 @@ export default function SignUpForm() {
           placeholder="Password"
           inputClassName="focus:!ring-0 placeholder:text-[#6B7280]"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value), handleInputChange();
+          }}
         />
         <span
           className="absolute bottom-3 right-4 cursor-pointer text-[#6B7280] rtl:left-4 rtl:right-auto sm:bottom-3.5"
@@ -140,8 +166,9 @@ export default function SignUpForm() {
         containerClassName="!items-start"
         inputClassName="mt-1 focus:!ring-offset-[1px]"
         size="sm"
-        // checked={agreeChecked}
-        // onChange={(e) => setAgreeChecked(e.target.checked)}
+        checked={agreeChecked}
+        onChange={(e) => setAgreeChecked(e.target.checked)}
+        disabled={!allFieldsFilled}
       />
       <Button
         type="submit"
