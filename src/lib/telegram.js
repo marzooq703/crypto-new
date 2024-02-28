b64077dbb2b5e56012e5e5bcf2be3de66abbfb87
unknown <tariqahmed1102@gmail.com>
2024-02-28 13:46:33 +0530

    have implemented telegram thing but not finished

--- src/lib/telegram.js
+++ src/lib/telegram.js
@@ -1,7 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Initialize your Telegram Bot with the API token obtained from BotFather
-const bot = new TelegramBot('YOUR_TELEGRAM_BOT_TOKEN', { polling: true });
+const bot = new TelegramBot('7035310993:AAGnSwYQ5NOuK22q7Z4c6NoTauZ2w6O99q0', {
+  polling: true,
+}); // bot token should be added.
 
 // Define a function to send a message to a user
 const sendMessageToUser = async (chatId, message) => {
@@ -13,9 +15,18 @@ const sendMessageToUser = async (chatId, message) => {
   }
 };
 
-// Define a function to redirect a user to a Telegram group
+// // Define a function to redirect a user to a Telegram group
 function redirectToGroup(chatId, groupLink) {
   bot.sendMessage(chatId, `Join our Telegram group: ${groupLink}`);
 }
 
-module.exports = { sendMessageToUser, redirectToGroup };
+const sendMessageToGroup = async (groupChatId, message) => {
+  try {
+    await bot.sendMessage(groupChatId, message);
+    console.log('Message sent successfully');
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
+};
+
+module.exports = { sendMessageToUser, redirectToGroup, sendMessageToGroup };
