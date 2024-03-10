7995efc982c51f4d4183cdf43808885e5a82bc41
Hassan Marzooq <marzooq703@gmail.com>
2024-03-10 17:45:54 +0530

    completed telegram message connection

--- src/app/classic/telegram/api/route.js
+++ src/app/classic/telegram/api/route.js
@@ -1,4 +1,36 @@
-// export async function GET(request) {
+var axios = require('axios').default;
 
-//   return Response.json({ name: 'tariq' });
-// }
+const sendMessageToGroup = async (groupChatId, message) => {
+  try {
+    const TelegramBot = require('node-telegram-bot-api');
+    const bot = new TelegramBot(
+      '7035310993:AAGnSwYQ5NOuK22q7Z4c6NoTauZ2w6O99q0',
+      { polling: true },
+    );
+
+    await bot.sendMessage(groupChatId, message);
+    console.log('Message sent successfully to group');
+    return 'Message sent successfully to group';
+  } catch (error) {
+    console.error('Error sending message to group:', error);
+    return 'error';
+  }
+};
+
+export async function GET(request) {
+  try {
+    // const response = await axios.request(options);
+    // console.log('telegram request initiated:', response.data);
+
+    // Send message to group via Telegram after KYC request is made
+    const groupChatId = '-4140541763'; // Replace with your group chat ID
+    const message = 'telegram request has been initiated.';
+    const finalResp = await sendMessageToGroup(groupChatId, message);
+
+    return Response.json({ data: finalResp });
+  } catch (error) {
+    console.error('Error initiating telegram request:', error);
+    return Response.error({ message: error.message });
+  }
+  //   return Response.json({ name: 'tariq' });
+}
--- src/app/classic/telegram/route.js
+++ src/app/classic/telegram/route.js
@@ -1,19 +1,50 @@
+var axios = require('axios').default;
+
+const sendMessageToGroup = async (groupChatId, message) => {
+  try {
+    const TelegramBot = require('node-telegram-bot-api');
+    const bot = new TelegramBot('YOUR_BOT_TOKEN', { polling: true });
+
+    await bot.sendMessage(groupChatId, message);
+    console.log('Message sent successfully to group');
+  } catch (error) {
+    console.error('Error sending message to group:', error);
+  }
+};
+
+export async function GET(request) {
+  const options = {
+    method: 'post',
+    url: 'https://dg-sandbox.setu.co/api/okyc',
+    headers: {
+      'x-client-id': 'bfa20161-1440-4cf9-807f-bb5eb96a4d8a',
+      'x-client-secret': 'fQ9bex8f2Ttqb6Mox3w2qQe6VUITQlIV',
+      'x-product-instance-id': 'f3d073fe-bec8-4739-9b75-f650cc406a8d',
+    },
+    data: {
+      redirectURL: 'http://localhost:3000/classic/telegram',
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log('telegram request initiated:', response.data);
+
+    // Send message to group via Telegram after KYC request is made
+    // const groupChatId = '-4140541763'; // Replace with your group chat ID
+    const message = 'telegram request has been initiated.';
+    await sendMessageToGroup(groupChatId, message);
+
+    return Response.json({ data: response.data });
+  } catch (error) {
+    console.error('Error initiating telegram request:', error);
+    return Response.error({ message: error.message });
+  }
+}
 // var axios = require('axios').default;
 
-// const sendMessageToGroup = async (groupChatId, message) => {
-//   try {
-//     const TelegramBot = require('node-telegram-bot-api');
-//     const bot = new TelegramBot('YOUR_BOT_TOKEN', { polling: true });
-
-//     await bot.sendMessage(groupChatId, message);
-//     console.log('Message sent successfully to group');
-//   } catch (error) {
-//     console.error('Error sending message to group:', error);
-//   }
-// };
-
 // export async function GET(request) {
-//   const options = {
+//   var options = {
 //     method: 'post',
 //     url: 'https://dg-sandbox.setu.co/api/okyc',
 //     headers: {
@@ -22,50 +53,19 @@
 //       'x-product-instance-id': 'f3d073fe-bec8-4739-9b75-f650cc406a8d',
 //     },
 //     data: {
-//       redirectURL: 'http://localhost:3000/classic/telegram',
+//       redirectURL: 'http://localhost:3000/classic/kyc',
 //     },
 //   };
+//   let res = await axios
+//     .request(options)
+//     .then(function (response) {
+//       console.log(response.data);
+//       return response.data;
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//       return error;
+//     });
 
-//   try {
-//     const response = await axios.request(options);
-//     console.log('telegram request initiated:', response.data);
-
-//     // Send message to group via Telegram after KYC request is made
-//     // const groupChatId = '-4140541763'; // Replace with your group chat ID
-//     const message = 'telegram request has been initiated.';
-//     await sendMessageToGroup(groupChatId, message);
-
-//     return Response.json({ data: response.data });
-//   } catch (error) {
-//     console.error('Error initiating telegram request:', error);
-//     return Response.error({ message: error.message });
-//   }
+//   return Response.json({ res });
 // }
-// // var axios = require('axios').default;
-
-// // export async function GET(request) {
-// //   var options = {
-// //     method: 'post',
-// //     url: 'https://dg-sandbox.setu.co/api/okyc',
-// //     headers: {
-// //       'x-client-id': 'bfa20161-1440-4cf9-807f-bb5eb96a4d8a',
-// //       'x-client-secret': 'fQ9bex8f2Ttqb6Mox3w2qQe6VUITQlIV',
-// //       'x-product-instance-id': 'f3d073fe-bec8-4739-9b75-f650cc406a8d',
-// //     },
-// //     data: {
-// //       redirectURL: 'http://localhost:3000/classic/kyc',
-// //     },
-// //   };
-// //   let res = await axios
-// //     .request(options)
-// //     .then(function (response) {
-// //       console.log(response.data);
-// //       return response.data;
-// //     })
-// //     .catch(function (error) {
-// //       console.error(error);
-// //       return error;
-// //     });
-
-// //   return Response.json({ res });
-// // }
