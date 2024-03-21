d079ce447b94a0ce717268511f9718ada1b0baf8
Hassan Marzooq <marzooq703@gmail.com>
2024-03-21 10:19:46 +0530

    added parms for Telegram API post request

--- src/app/classic/telegram/api/route.js
+++ src/app/classic/telegram/api/route.js
@@ -1,13 +1,11 @@
 var axios = require('axios').default;
 
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot('7035310993:AAGnSwYQ5NOuK22q7Z4c6NoTauZ2w6O99q0', {
+  polling: true,
+});
 const sendMessageToGroup = async (groupChatId, message) => {
   try {
-    const TelegramBot = require('node-telegram-bot-api');
-    const bot = new TelegramBot(
-      '7035310993:AAGnSwYQ5NOuK22q7Z4c6NoTauZ2w6O99q0',
-      { polling: true },
-    );
-
     await bot.sendMessage(groupChatId, message);
     console.log('Message sent successfully to group');
     return 'Message sent successfully to group';
@@ -17,14 +15,15 @@ const sendMessageToGroup = async (groupChatId, message) => {
   }
 };
 
-export async function GET(request) {
+export async function POST(request) {
   try {
-    // const response = await axios.request(options);
-    // console.log('telegram request initiated:', response.data);
+    const data = await request.json();
+    console.log(data);
+    const { type, amount, userName, userID, city } = data;
 
     // Send message to group via Telegram after KYC request is made
     const groupChatId = '-4140541763'; // Replace with your group chat ID
-    const message = 'telegram request has been initiated.';
+    const message = `${type} request has been raised for the amount of ${amount} for ${userName}[${userID}] from ${city}`;
     const finalResp = await sendMessageToGroup(groupChatId, message);
 
     return Response.json({ data: finalResp });
