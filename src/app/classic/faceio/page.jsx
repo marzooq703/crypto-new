8dc09572fb0a6a82eff515fd2a7bd4797d40689f
Aqeel703 <ahamed703aqeel@gmail.com>
2024-03-25 18:55:32 +0530

    added the face io in the node js

--- src/app/classic/faceio/page.jsx
+++ src/app/classic/faceio/page.jsx
@@ -1,15 +1,9 @@
 'use client';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Button from '@/components/ui/button/button';
-import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation';
-import { getFirestore } from 'firebase/firestore';
-import { db } from '../../../lib/firebase';
 import faceIO from '@faceio/fiojs';
 
-const faceio = new faceIO('fd07583c1f6f3034dcf990990138c461'); // Get the application Public ID at https://console.faceio.net.
-const API_KEY = 'fd07583c1f6f3034dcf990990138c461';
+const faceio = new faceIO('fioac267'); // Get the application Public ID at https://console.faceio.net.
+const API_KEY = 'fioac267';
 function App() {
   return (
     <div className="App">
@@ -21,25 +15,21 @@ function App() {
 
 async function authenticateUser() {
   try {
-    const response = await axios.post(
-      'https://api.faceio.net/authenticate',
-      {
-        locale: 'auto', // Default user locale
-      },
-      {
-        headers: {
-          'WWW-Authenticate': `Bearer ${API_KEY}`,
-        },
-      },
-    );
-
-    const userData = response.data;
+    const userData = await faceio.authenticate({
+      locale: 'auto', // Default user locale
+    });
 
     console.log('Success, user identified');
-    // Handle user data as needed
+    // Grab the facial ID linked to this particular user which will be same
+    // for each of his successful future authentication. FACEIO recommend
+    // that your rely on this Facial ID if you plan to uniquely identify
+    // all enrolled users on your backend for example.
+    console.log('Linked facial Id: ' + userData.facialId);
+    // Grab the arbitrary data you have already linked (if any) to this particular user
+    // during his enrollment via the payload parameter of the enroll() method.
+    console.log('Payload: ' + JSON.stringify(userData.payload)); // {"whoami": 123456, "email": "john.doe@example.com"} from the enroll() example above
   } catch (error) {
-    console.log(error);
-    handleError(error?.response?.data?.code);
+    handleError(error.code);
   }
 }
 
@@ -93,24 +83,14 @@ function handleError(errCode) {
 
 async function enrollNewUser() {
   try {
-    const response = await axios.post(
-      'https://api.faceio.net/enroll',
-      {
-        locale: 'auto', // Default user locale
-        payload: {
-          /* The payload we want to associate with this particular user which is forwarded back to us upon future authentication of this user.*/
-          whoami: 123456, // Dummy ID linked to this particular user
-          email: 'ahamed703aqeel@gmail.com',
-        },
+    const userInfo = await faceio.enroll({
+      locale: 'auto', // Default user locale
+      payload: {
+        /* The payload we want to associate with this particular user which is forwarded back to us upon future authentication of this user.*/
+        whoami: 123423, // Dummy ID linked to this particular user
+        email: 'ahamedaqeel703@gmail.com',
       },
-      {
-        headers: {
-          'WWW-Authenticate': `Bearer ${API_KEY}`,
-        },
-      },
-    );
-
-    const userInfo = response.data;
+    });
 
     alert(
       `User Successfully Enrolled! Details:
@@ -120,7 +100,7 @@ async function enrollNewUser() {
           Age Approximation: ${userInfo.details.age}`,
     );
   } catch (error) {
-    handleError(error.response.data.code);
+    handleError(error.code);
   }
 }
 
