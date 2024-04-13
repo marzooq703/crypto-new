ae1f2ec05b584e91af0526c6797578e270a70601
Hassan Marzooq <marzooq703@gmail.com>
2024-04-13 15:02:35 +0530

    added useclient on payment confirmation

--- src/app/classic/payment-confirmation/page.jsx
+++ src/app/classic/payment-confirmation/page.jsx
@@ -1,10 +1,25 @@
+"use client";
 import {  useSearchParams } from 'next/navigation';
+import axios from 'axios;';
+import { useEffect } from 'react';
+
 const PaymentConfirmation = () => {
     const searchParams = useSearchParams();
     const orderId = searchParams.get('orderid');
+    // useEffect(async () => {
+    //     const response = await axios.get(
+    //         'https://api.coingecko.com/api/v3/simple/price',
+    //         {
+    //           params: {
+    //             ids: 'tether',
+    //             vs_currencies: toCurrency === 'inr' ? 'inr' : 'usd',
+    //           },
+    //         },
+    //       );
+    // }, [])
     return (
         <>
-        <div>Payment Confirmation</div> - {orderId}
+            <div>Payment Confirmation</div> - {orderId}
         </>
     )
 }
