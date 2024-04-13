e430c9a0a5cf6f7943dd714e8451c7bb423fd99e
Hassan Marzooq <marzooq703@gmail.com>
2024-04-13 15:16:25 +0530

    added if block

--- src/app/classic/payment-confirmation/page.jsx
+++ src/app/classic/payment-confirmation/page.jsx
@@ -1,17 +1,22 @@
-"use client";
+'use client';
 import {  useSearchParams } from 'next/navigation';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const PaymentConfirmation = () => {
     const searchParams = useSearchParams();
     const orderId = searchParams.get('orderid');
+    const [paymentResponse, setPaymentResponse] = useState({});
     useEffect(async () => {
         if(orderId) {
             const response = await axios.get(
                 `https://cr-backend-three.vercel.app/api/cashfree/payments/${orderId}`
               );
-              console.log(response)
+              console.log(response);
+              setPaymentResponse(response[0])
+              if(response[0].payment_status == 'SUCCESS') {
+
+              }
         }
     }, []);
     return (
