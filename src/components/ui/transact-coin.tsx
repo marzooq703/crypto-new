a6ae264b2424a38830d5ef04e80d6c4699b1c5ca
Hassan Marzooq <marzooq703@gmail.com>
2024-04-14 09:20:29 +0530

    got buy and sell data from server on dashboard

--- src/components/ui/transact-coin.tsx
+++ src/components/ui/transact-coin.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/navigation';
 import cn from 'classnames';
 import Button from '@/components/ui/button';
 import axios from 'axios';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../lib/firebase';
 
 type TransactionType = 'buy' | 'sell';
 
@@ -17,40 +19,34 @@ const TransactCoin = ({ className, transactionType }: TransactCoinProps) => {
   const [conversionRate, setConversionRate] = useState(0);
   const [fromCurrency, setFromCurrency] = useState<'usdt' | 'inr'>('usdt');
   const [toCurrency, setToCurrency] = useState<'usdt' | 'inr'>('inr');
+  const [buyValue, setBuyValue] = useState(0);
+  const [sellValue, setSellValue] = useState(0);
   const decimalPattern = /^[0-9]*[.,]?[0-9]*$/;
   const inrToUsdtRate = 0.014;
 
   useEffect(() => {
     const fetchConversionRate = async () => {
       try {
-        const response = await axios.get(
-          'https://api.coingecko.com/api/v3/simple/price',
-          {
-            params: {
-              ids: 'tether',
-              vs_currencies: toCurrency === 'inr' ? 'inr' : 'usd',
-            },
+        const unsub = onSnapshot(doc(db, 'currentPricing', 'Buy'), (doc) => {
+          const data = doc.data();
+          console.log('Current data: ', data);
+          const buy = data?.current;
+          setBuyValue(buy);
+        });
+        const sellValue = onSnapshot(
+          doc(db, 'currentPricing', 'Sell'),
+          (doc) => {
+            const data = doc.data();
+            console.log('Current data: ', data);
+            const sell = data?.current;
+            setSellValue(sell);
           },
         );
-
-        if (
-          response.data &&
-          response.data.tether &&
-          response.data.tether[toCurrency === 'inr' ? 'inr' : 'usd']
-        ) {
-          const conversionRate =
-            response.data.tether[toCurrency === 'inr' ? 'inr' : 'usd'];
-          setConversionRate(conversionRate);
-        } else {
-          throw new Error('Invalid response or missing data');
-        }
-      } catch (error) {
-        console.error('Error fetching conversion rate:', error);
+      } catch (err) {
+        console.error(err);
       }
     };
-
-    fetchConversionRate();
-  }, [transactionType, toCurrency]); // Update conversion rate when transaction type or currency changes
+  }, []); // Update conversion rate when transaction type or currency changes
 
   // Update the handleOnChangeFirstCoin function to ensure it only accepts valid numerical values
   const handleOnChangeFirstCoin = (
@@ -133,10 +129,18 @@ const TransactCoin = ({ className, transactionType }: TransactCoinProps) => {
           {(amount * conversionRate).toFixed(4)}
         </span>
       </div> */}
-      <div className="relative mt-4 flex h-11 w-full items-center justify-between rounded-lg border border-gray-100 bg-body px-4 pl-3 text-sm text-gray-900 dark:border-gray-700 dark:bg-light-dark dark:text-white sm:h-13 sm:pl-4">
-        <span className="relative flex items-center gap-3 font-medium">Buy: 91</span>
-        <span className="relative flex items-center gap-3 font-medium">Sell: 90</span>
-</div>
+      {buyValue && sellValue ? (
+        <div className="relative mt-4 flex h-11 w-full items-center justify-between rounded-lg border border-gray-100 bg-body px-4 pl-3 text-sm text-gray-900 dark:border-gray-700 dark:bg-light-dark dark:text-white sm:h-13 sm:pl-4">
+          <span className="relative flex items-center gap-3 font-medium">
+            Buy: {buyValue}
+          </span>
+          <span className="relative flex items-center gap-3 font-medium">
+            Sell: {sellValue}
+          </span>
+        </div>
+      ) : (
+        'Loading...'
+      )}
       {/* Button */}
       <Button
         size="large"
