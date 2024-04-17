8f799fd7b5a7e5852c1d3c0924995853f42f5b69
Aqeel703 <ahamed703aqeel@gmail.com>
2024-04-18 04:35:49 +0530

    added the hash read in the erc 20 added the useWaitForTransactionReceipt

--- src/app/classic/erc20/page.jsx
+++ src/app/classic/erc20/page.jsx
@@ -1,28 +1,38 @@
 'use client';
 
-import { useConnect, useAccount, useWriteContract } from 'wagmi';
+import {
+  useConnect,
+  useAccount,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+} from 'wagmi';
 import { injected } from 'wagmi/connectors';
 import { useState } from 'react';
 import { polygon } from 'viem/chains';
+import { useBalance } from 'wagmi';
 
 export const PayButton = () => {
   const { connectAsync } = useConnect();
   const { address } = useAccount();
   const { writeContractAsync } = useWriteContract();
+  const [hashs, setHashs] = useState('');
+
+  const { isLoading, isSuccess, error } = useWaitForTransactionReceipt({
+    hash: hashs,
+  });
+  console.log(isLoading, 'isLoading');
+  console.log(isSuccess, 'isSuccess');
+  console.log(error, 'error');
   const [started, setStarted] = useState(false);
-  const [errors, setErrors] = useState('');
   const [completed, setCompleted] = useState(false);
   const price = 0.1;
 
   const handlePayment = async () => {
     try {
-      setErrors('');
       setStarted(true);
-
       if (!address) {
         await connectAsync({ chainId: polygon.id, connector: injected() });
       }
-
       const data = await writeContractAsync({
         chainId: polygon.id,
         address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
@@ -104,38 +114,44 @@ export const PayButton = () => {
         ],
         args: ['0x269b7Fb9F7Be8945E6d0fD5c132E86c79ab55D2B', price * 1000000],
       });
-
       setCompleted(true);
+      setHashs(data);
+      console.log(`https://polygonscan.com/tx/${data}`);
+      console.log(hashs);
       console.log(data);
     } catch (err) {
       console.error(err);
       setStarted(false);
-      setErrors('Payment failed. Please try again: ' + err.message);
-      setCompleted(false); 
+      setCompleted(false);
     }
   };
 
+  if (isLoading) {
+    return <p>Waiting for confirmation...</p>;
+  }
+
+  if (isSuccess) {
+    return <p>Transaction confirmed.</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <>
-      {!completed && (
-        <button
-          disabled={started}
-          className="mt-5 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={handlePayment}
+      <button onClick={handlePayment} disabled={started || completed}>
+        {started ? 'Confirming...' : 'Pay Now'}
+      </button>
+      {/* <h1>
+        <a
+          href={`https://polygonscan.com/address/${hashs}`}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {started ? 'Confirming...' : 'Pay Now'}
-        </button>
-      )}
-      {completed && (
-        <p className="text-stone-800 mt-2 bg-green-200 rounded-md text-sm py-2 px-4">
-          Thank you for your payment.
-        </p>
-      )}
-      {errors && (
-        <p className="text-stone-800 mt-2 bg-red-200 rounded-md text-sm py-2 px-4">
-          {errors}
-        </p>
-      )}
+          View Transaction on PolygonScan
+        </a>
+      </h1> */}
     </>
   );
 };
