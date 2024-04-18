232a7937f0b89c4c9dbade19974e94243f2aec7a
Hassan Marzooq <marzooq703@gmail.com>
2024-04-18 21:05:28 +0530

    Redirected authorisation on not logged in

--- src/app/classic/layout.tsx
+++ src/app/classic/layout.tsx
@@ -1,9 +1,16 @@
 import ClassicLayout from '@/layouts/classic/layout';
-// import { useEffect } from 'react';
-// import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // const router = useRouter();
-
+  const router = useRouter();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const user = localStorage.getItem('crypto-user');
+      if (!user) {
+        router.push('/authentication');
+      }
+    }
+  }, [router]);
   return <ClassicLayout>{children}</ClassicLayout>;
 }
